fd14217f2736a7c7ea2842c61349715eadded09e
Matheus Soutto <matheussoutto27@gmail.com>
2022-05-25 21:50:09 +0000

    Setting up vite to deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
+import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
-export default {
-  plugins: [reactRefresh()],
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      port: 443,
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      plugins: [reactRefresh()],
+      server: {
+        host: '0.0.0.0',
+        hmr: {
+          port: 443,
+        }
+      }
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      base: '/portal-salve-web/'
     }
   }
-}
+  
+});
